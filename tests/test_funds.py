008f579eb62484f1f8747d72a90336820c3686b2
andrey-jef <a.jef@simplelogin.co>
2023-12-15 09:55:17 +0100

    test: update test cases

--- tests/test_funds.py
+++ tests/test_funds.py
@@ -1,27 +1,35 @@
+import pytest
 import pandas as pd
-from vnstock.funds import funds_listing
+from vnstock.funds import *
 
-def assert_valid_dataframe(result):
+def assert_is_dataframe(result):
     assert isinstance(result, pd.DataFrame)
     assert len(result) > 0
 
-class TestFundsListing:
-    # Retrieve list of available funds with default parameters
-    def test_default_parameters(self):
-        result = funds_listing()
-        assert_valid_dataframe(result)
-    
-    # Retrieve list of available funds with all parameters specified
-    def test_all_parameters_specified(self):
-        result = funds_listing(lang='en', fund_type="STOCK", mode="full", decor=False)
-        assert_valid_dataframe(result)
-    
-    # Retrieve list of available funds with unsupported language
-    def test_unsupported_language(self):
-        result = funds_listing(lang='fr')
-        assert_valid_dataframe(result)
-    
-    # Retrieve list of available funds with unsupported fund type
-    def test_unsupported_fund_type(self):
-        result = funds_listing(fund_type="INVALID")
-        assert_valid_dataframe(result)
+# Retrieve dataframe with default parameters
+def test_default_parameters():
+    result1 = funds_listing()
+    result2 = fund_details()
+    result3 = fund_filter()
+    result4 = fund_top_holding()
+    result5 = fund_industry_holding()
+    result6 = fund_nav_report()
+    result7 = fund_asset_holding()
+
+    assert_is_dataframe(result1)
+    assert_is_dataframe(result2)
+    assert_is_dataframe(result3)
+    assert_is_dataframe(result4)
+    assert_is_dataframe(result5)
+    assert_is_dataframe(result6)
+    assert_is_dataframe(result7)
+
+# Retrieve dataframe with all parameters specified
+def test_all_parameters_specified():
+    result = funds_listing(lang='en', fund_type="STOCK", mode="full", decor=False)
+    assert_is_dataframe(result)
+
+# Retrieve dataframe with unsupported param input
+def test_unsupported_param_input():
+    result = funds_listing(fund_type="INVALID")
+    assert_is_dataframe(result)
