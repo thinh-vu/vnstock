746659caaac0a7079a1f3ed5793ab02a9635c5d8
Thinh Vu <39723360+thinh-vu@users.noreply.github.com>
2024-04-01 21:42:13 +0000

    update docs

--- vnstock/integration.py
+++ vnstock/integration.py
@@ -486,22 +486,23 @@ def telegram_send_photo(token_key, chat_id, message, file_path):
     file_name = file_path.split(get_path_delimiter())[-1]
     file_type = file_name.split('.')[-1]
     files = [('photo', (file_name, open(file_path, 'rb'), 'image/{}'.format(file_type)))]
-    url = 'https://api.telegram.org/{}/sendPhoto'.format(token_key)
+    url = 'https://api.telegram.org/bot{}/sendPhoto'.format(token_key)
     payload = {'chat_id': chat_id, 'caption': message}
     response = requests.post(url, headers={}, data=payload, files=files)
     return response
 
 def telegram_send_message(token_key, chat_id, message):
-    """
-    Send a message to a Telegram group.
+	"""
+	Send a message to a Telegram group.
 
-    Args:
-        token_key (str): Telegram token key.
-        chat_id (str): ID of the target Telegram channel/group, e.g., '-1001439492355'.
-        message (str): Your text message.
+	Args:
+		token_key (str): Telegram token key.
+		chat_id (str): ID of the target Telegram channel/group, e.g., '-1001439492355'.
+		message (str): Your text message.
 
-    Returns:
-        object: Response object from the Telegram API.
-    """
-    tel_url = 'https://api.telegram.org/{}/sendMessage?chat_id={}&text={}'.format(token_key, chat_id, message)
-    return requests.post(tel_url)
+	Returns:
+		object: Response object from the Telegram API.
+	"""
+	tel_url = 'https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'.format(token_key, chat_id, message) # thêm chữ bot trước token so với code cũ
+	response = requests.post(tel_url)
+	return response.json()
