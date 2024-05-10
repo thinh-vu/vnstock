b440bc1c84692dd477d526d4619456b2e8a171e0
Thinh Vu <39723360+thinh-vu@users.noreply.github.com>
2024-05-10 16:08:01 +0000

    adjust setup

---
+++ vnstock3/common/__init__.py
@@ -0,0 +1,3 @@
+from vnstock3.common.data.data_explorer import Vnstock, Quote, Listing, Trading, Company, Finance, MSNComponents
+from vnstock3.core.utils.env import id_valid
+id_valid()
--- vnstock3/common/data/__init__.py
+++
@@ -1,3 +0,0 @@
-from .data_explorer import Vnstock, Quote, Listing, Trading, Company, Finance, MSNComponents
-from vnstock3.core.utils.env import id_valid
-id_valid()
