894223b70f9cef9a8671d18be1e7cfb1acf03894
thinh-vu <mrthinh360@gmail.com>
2025-03-18 23:26:38 +0700

    applied bug fix for pr154

--- vnstock/common/vnstock.py
+++ vnstock/common/vnstock.py
@@ -38,11 +38,14 @@ class Vnstock:
         if symbol is None:
             self.symbol = 'VN30F1M'
             logger.info("Mã chứng khoán không được chỉ định, chương trình mặc định sử dụng VN30F1M")
-
-        if source is None:
-            source = self.source
         else:
-            self.symbol = symbol
+             self.symbol = symbol
+ 
+         if source is None:
+             source = self.source
+         else:
+             self.symbol = symbol
+             
         return StockComponents(self.symbol, source, show_log=self.show_log)
     
     def fx(self, symbol: Optional[str]='EURUSD', source: Optional[str] = "MSN"):
