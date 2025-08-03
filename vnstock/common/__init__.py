30da6191fdbe370e8e7bbcddd60b432da2800977
thinh-vu <mrthinh360@gmail.com>
2025-08-04 00:08:19 +0700

    sửa lỗi chức năng visualization, thêm badge mở notebook minh hoạ từ nhiều platform thay thế colab

--- vnstock/common/__init__.py
+++ vnstock/common/__init__.py
@@ -1,3 +1,4 @@
+from vnstock.common.plot import chart_wrapper
 from vnstock.core.utils.env import id_valid
 from vnstock.core.utils.upgrade import update_notice
 from vnstock.common.data.data_explorer import StockComponents, MSNComponents, Quote, Listing, Trading, Company, Finance, Screener, Fund
