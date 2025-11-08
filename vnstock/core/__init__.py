2cbdc2ec03e5febe2666b3b3cdc08610db5ea621
Thinh Vu <mrthinh360@gmail.com>
2025-11-08 10:44:24 +0700

    remove vnai.setup() call to prevent circular import issues

--- vnstock/core/__init__.py
+++ vnstock/core/__init__.py
@@ -3,4 +3,5 @@ from .utils.parser import *
 from .utils.logger import *
 from .utils.env import *
 
-vnai.setup()
+# Note: vnai.setup() is called in vnstock/__init__.py after all imports are complete
+# to avoid circular import issues
